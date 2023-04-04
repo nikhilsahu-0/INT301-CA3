2e810cbc7475e99ffd122d66bd4deeb6dbf295d0
Nikhil Sahu <nikhil73943@gmail.com>
2023-04-04 21:29:29 +0530

    add nodejs server code

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const net = require('net');
+const express = require("express");
+const app = express();
 
-const serverAddress = '192.168.0.1';
-const serverPort = 8080; 
+app.get("/", (req, res) => {
+  res.send("TESTING SERVER");
+});
 
-const socket = new net.Socket();
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+  ];
+  res.json(users);
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username === "admin" && password === "password") {
+    res.send("Login successful");
+  } else {
+    res.status(401).send("Incorrect username or password");
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
