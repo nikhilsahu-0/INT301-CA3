6eef6bcaf6860351dd927f5befc7211a3726217e
Nikhil Sahu <nikhil73943@gmail.com>
2023-04-05 20:53:05 +0530

    add endpoints

--- script.js
+++ script.js
@@ -23,6 +23,19 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  // Update user with given ID
+  res.send(`User with ID ${id} updated to name ${name}`);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  // Delete user with given ID
+  res.send(`User with ID ${id} deleted`);
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
